4b0206d92d28f7c1fb3457c0684211361317fc2b
Weslei Tiago <weslei.tiago123@gmail.com>
2023-03-01 10:29:50 -0300

    finding a product searching by name

---
+++ cloud/main.js
@@ -0,0 +1,36 @@
+const Product = Parse.Object.extend("Product");
+
+Parse.Cloud.define("hello", (req) => {
+	return "Hello world from mercadinho";
+});
+
+Parse.Cloud.define("get-product-list", async (req) => {
+    const queryProducts = new Parse.Query(Product);
+
+    //condições da query
+
+    if(req.params.title != null){
+        queryProducts.fullText('title', req.params.title);
+        //queryProducts.matches('title', '.*' + req.params.title + '.*');
+    }
+
+    const itemsPerPage = req.params.itemsPerPage || 20;
+    if(itemsPerPage > 50) throw "Quantidade inválida de itens por página";
+
+    queryProducts.skip(itemsPerPage * req.params.page || 0);
+    queryProducts.limit(itemsPerPage);
+
+    const resultProducts = await queryProducts.find({useMasterKey: true});
+
+    return resultProducts.map(function (p) {
+        p = p.toJSON();
+        return {
+            id: p.objectId,
+            title: p.title,
+            description: p.description,
+            price: p.price,
+            unit: p.unit,
+            picture: p.picture != null ? p.picture.url : null,
+        }
+    });
+});
